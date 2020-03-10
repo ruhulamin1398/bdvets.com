2a0cf3e6efbe0656af4f4ae0057c4cb121f5394b
Rul <ruhul.ok@gmail.com>
2020-03-10 21:57:21 +0600

    medichine

--- database/migrations/2010_03_10_002329_create_medichine_categories_table.php
+++ database/migrations/2010_03_10_002329_create_medichine_categories_table.php
@@ -15,6 +15,8 @@ class CreateMedichineCategoriesTable extends Migration
     {
         Schema::create('medichine_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
