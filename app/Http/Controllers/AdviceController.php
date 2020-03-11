55ff1ea6e0c41a688bd962a3095becd21562db72
Rul <ruhul.ok@gmail.com>
2020-03-11 20:46:18 +0600

    model

---
+++ app/Http/Controllers/AdviceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\advice;
+use Illuminate\Http\Request;
+
+class AdviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\advice  $advice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(advice $advice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\advice  $advice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(advice $advice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\advice  $advice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, advice $advice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\advice  $advice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(advice $advice)
+    {
+        //
+    }
+}
